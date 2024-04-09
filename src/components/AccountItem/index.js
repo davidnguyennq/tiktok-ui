d0b5024cfbe9472f02feffb7c944b75b4621dbe8
nhatquansan <nguyennhatquan.dev@gmail.com>
2024-04-09 22:48:37 +0700

    header-logic-v1

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -2,13 +2,14 @@ import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
 import styles from './AccountItem.module.scss';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/510b26e180d28092db2fe997210c1b82~c5_300x300.webp?lk3s=a5d48078&amp;x-expires=1712628000&amp;x-signature=WmlY5s6UXt%2BywmWunQN4E3rNKoA%3D"
                 alt="Hoaa"
